8745b4d4114caffc7c3134a3c242b2bed1be7084
saumitra1993 <theapogee2011@gmail.com>
2019-06-14 12:53:58 +0530

    added pos pwa

---
+++ pospwa/src/POSAPI.js
@@ -0,0 +1,571 @@
+export const stubItems = [
+  {
+    di: "80fdb9ca-028b-4a50-9b8b-f7b2517f5039",
+    variants: '{" Large \\/ Stone":19322181091395}',
+    cart: "0bbb8650ead45ce81b11c502fa9c35bb",
+    dt: "Alia Knit Trouser",
+    et: 3,
+    act: true,
+    empi: 2109627793475,
+    ts: 1555311103055,
+    du: "http://aadhar-prod-store.myshopify.com/products/alia-knit-trouser",
+    pid: "BUtzoEqjdeDvGAn2Ay8D45+8hwslVdSWdEBwiVoU/60=",
+    epi: 19322181091395,
+    id: "a1a1ae3c-5f88-4e4b-a1ae-3c5f881e4b22",
+    iu:
+      "https://cdn.shopify.com/s/files/1/0038/3415/0979/products/2015-02-05_Ashley_Look_38_30114_2540.jpg?v=1555308362",
+    qty: 1,
+    dcat1: "Personal computer",
+    pr: 607.6,
+    ct: "women's pants",
+    dcat: "Mac",
+    hashtags: ["test"]
+  },
+  {
+    di: "80fdb9ca-028b-4a50-9b8b-f7b2517f5039",
+    pt: "Alia Knit Trouser – Aadhar Prod Store",
+    rc: "default",
+    variants: '[{"X Small / Stone":19322180927555}]',
+    dt: "Alia Knit Trouser",
+    et: 1,
+    act: true,
+    empi: 2109627793475,
+    ts: 1555311089030,
+    du: "https://aadhar-prod-store.myshopify.com/products/alia-knit-trouser",
+    pid: "BUtzoEqjdeDvGAn2Ay8D45+8hwslVdSWdEBwiVoU/60=",
+    epi: 19322180927555,
+    id: "5e4ef1ab-3a2c-4221-8ef1-ab3a2ce221d6",
+    ru: "https://aadhar-prod-store.myshopify.com/collections/all",
+    iu:
+      "//cdn.shopify.com/s/files/1/0038/3415/0979/products/2015-02-05_Ashley_Look_38_30114_2540.jpg?v=1555308362",
+    dcat1: "Personal computer",
+    pr: 607.6,
+    ct: "women's pants",
+    dcat: "Mac"
+  },
+  {
+    di: "80fdb9ca-028b-4a50-9b8b-f7b2517f5039",
+    pt: "Zepo Brushed Cotton Blazer – Aadhar Prod Store",
+    rc: "default",
+    variants: '[{"Medium / Anchovy":19322247479363}]',
+    dt: "Zepo Brushed Cotton Blazer",
+    et: 1,
+    act: true,
+    empi: 2109630414915,
+    ts: 1555311070461,
+    du:
+      "https://aadhar-prod-store.myshopify.com/products/zepo-blazer-in-anchovy",
+    pid: "BUtzoEqjdeDvGAn2Ay8D45+8hwslVdSWdEBwiVoU/60=",
+    epi: 19322247479363,
+    id: "ee471e2a-2faf-44a3-871e-2a2faf74a314",
+    ru:
+      "https://aadhar-prod-store.myshopify.com/collections/all?page=1&sort_by=title-descending",
+    iu:
+      "//cdn.shopify.com/s/files/1/0038/3415/0979/products/1-26-15_Addis_Look_19_21852_1123.jpg?v=1555308738",
+    dcat1: "Personal computer",
+    pr: 439.6,
+    ct: "men's coats & jackets",
+    dcat: "Mac",
+    hashtags: ["blues"]
+  },
+  {
+    di: "80fdb9ca-028b-4a50-9b8b-f7b2517f5039",
+    pt: "Wool Crepe Pant – Aadhar Prod Store",
+    rc: "default",
+    variants: '[{"Black / 6":19321759006787}]',
+    dt: "Wool Crepe Pant",
+    et: 1,
+    act: true,
+    empi: 2109603086403,
+    ts: 1555310219187,
+    du: "https://aadhar-prod-store.myshopify.com/products/wool-crepe-pant",
+    pid: "BUtzoEqjdeDvGAn2Ay8D45+8hwslVdSWdEBwiVoU/60=",
+    epi: 19321759006787,
+    id: "f7869eac-1477-4879-869e-ac14770879b9",
+    ru:
+      "https://aadhar-prod-store.myshopify.com/collections/all?sort_by=title-descending",
+    iu:
+      "//cdn.shopify.com/s/files/1/0038/3415/0979/products/2015-06-11-Ashley_Look5_50094_23460.jpg?v=1555305111",
+    dcat1: "Personal computer",
+    pr: 758,
+    ct: "Womens Pants",
+    dcat: "Mac",
+    hashtags: ["test", "grease"]
+  },
+  {
+    di: "80fdb9ca-028b-4a50-9b8b-f7b2517f5039",
+    pt: "William Hat – Aadhar Prod Store",
+    rc: "default",
+    variants: '[{"Medium / Vintage Panama":19321746227267}]',
+    dt: "William Hat",
+    et: 1,
+    act: true,
+    empi: 2109601415235,
+    ts: 1555310218979,
+    du: "https://aadhar-prod-store.myshopify.com/products/william-hat",
+    pid: "BUtzoEqjdeDvGAn2Ay8D45+8hwslVdSWdEBwiVoU/60=",
+    epi: 19321746227267,
+    id: "e5f00eb3-3358-4a2f-b00e-b33358aa2ff3",
+    ru:
+      "https://aadhar-prod-store.myshopify.com/collections/all?sort_by=title-descending",
+    iu:
+      "//cdn.shopify.com/s/files/1/0038/3415/0979/products/2015-07-08_Laydown_50677_24512.jpg?v=1555304925",
+    dcat1: "Personal computer",
+    pr: 238,
+    ct: "women's hats",
+    dcat: "Mac"
+  },
+  {
+    di: "80fdb9ca-028b-4a50-9b8b-f7b2517f5039",
+    pt: "Washed Wool Blazer – Aadhar Prod Store",
+    rc: "default",
+    variants: '[{"48 / Grey":19321831292995}]',
+    dt: "Washed Wool Blazer",
+    et: 4,
+    act: true,
+    empi: 2109610197059,
+    ts: 1555310214065,
+    du:
+      "https://aadhar-prod-store.myshopify.com/products/washed-wool-blazer-grey",
+    pid: "BUtzoEqjdeDvGAn2Ay8D45+8hwslVdSWdEBwiVoU/60=",
+    epi: 19321831292995,
+    id: "2c023ae2-6ce1-43fe-823a-e26ce153fee0",
+    ru:
+      "https://aadhar-prod-store.myshopify.com/collections/all?page=2&sort_by=title-descending",
+    iu:
+      "//cdn.shopify.com/s/files/1/0038/3415/0979/products/2015-03-30_Jake_Look_12_23614_16486_cbceff6f-baf8-4af4-85e7-21905043e7c6.jpg?v=1555306051",
+    dcat1: "Personal computer",
+    pr: 1078,
+    ct: "men's coats & jackets",
+    dcat: "Mac"
+  },
+  {
+    di: "80fdb9ca-028b-4a50-9b8b-f7b2517f5039",
+    pt: "Washed Wool Blazer – Aadhar Prod Store",
+    rc: "default",
+    variants: '[{"48 / Grey":19321831292995}]',
+    dt: "Washed Wool Blazer",
+    et: 1,
+    act: true,
+    empi: 2109610197059,
+    ts: 1555310207465,
+    du:
+      "https://aadhar-prod-store.myshopify.com/products/washed-wool-blazer-grey",
+    pid: "BUtzoEqjdeDvGAn2Ay8D45+8hwslVdSWdEBwiVoU/60=",
+    epi: 19321831292995,
+    id: "08fa48a2-720c-4520-ba48-a2720c252082",
+    ru:
+      "https://aadhar-prod-store.myshopify.com/collections/all?page=2&sort_by=title-descending",
+    iu:
+      "//cdn.shopify.com/s/files/1/0038/3415/0979/products/2015-03-30_Jake_Look_12_23614_16486_cbceff6f-baf8-4af4-85e7-21905043e7c6.jpg?v=1555306051",
+    dcat1: "Personal computer",
+    pr: 1078,
+    ct: "men's coats & jackets",
+    dcat: "Mac"
+  },
+  {
+    di: "37b71b11-b746-4b8f-b539-34a046610f46",
+    pt: "Ally Ring – Engage-Test-Aadhar",
+    rc: "default",
+    variants: '[{"8 / Amethyst / Clear":16295366754338}]',
+    dt: "Ally Ring",
+    et: 1,
+    act: true,
+    empi: 1775534735394,
+    ts: 1553215123755,
+    du: "https://engage-test-aadhar.myshopify.com/products/ally-ring-amythest",
+    pid: "HUQiyMqiJnnnwJrxvLl+1dgTFlOvum7Rg/FnHdU/v7Y=",
+    epi: 16295366754338,
+    id: "8367763e-f330-47c2-a776-3ef33007c2b1",
+    ru: "https://engage-test-aadhar.myshopify.com/collections/all?page=2",
+    iu:
+      "//cdn.shopify.com/s/files/1/0048/8862/5186/products/2015-05-01_Accessories_03_23488_21508.jpg?v=1552993089",
+    dcat1: "Personal computer",
+    pr: 218,
+    ct: "women's rings",
+    dcat: "Mac"
+  },
+  {
+    di: "37b71b11-b746-4b8f-b539-34a046610f46",
+    pt: "Zig Coat in Evening – Engage-Test-Aadhar",
+    rc: "default",
+    variants: '[{"Evening / Large":16295793754146}]',
+    dt: "Zig Coat in Evening",
+    et: 4,
+    act: true,
+    empi: 1775621832738,
+    ts: 1553233538729,
+    du: "https://engage-test-aadhar.myshopify.com/products/zig-coat-evening",
+    pid: "HUQiyMqiJnnnwJrxvLl+1dgTFlOvum7Rg/FnHdU/v7Y=",
+    epi: 16295793754146,
+    id: "9121e4a1-d891-4cce-a1e4-a1d891fcce3b",
+    ru:
+      "https://engage-test-aadhar.myshopify.com/collections/all?sort_by=title-descending",
+    iu:
+      "//cdn.shopify.com/s/files/1/0048/8862/5186/products/2014-10-10_Addis_Look_03_03.jpg?v=1552997334",
+    dcat1: "Personal computer",
+    pr: 388,
+    ct: "men's coats & jackets",
+    dcat: "Mac"
+  },
+  {
+    di: "37b71b11-b746-4b8f-b539-34a046610f46",
+    pt: "A-Line Mini Dress in Blue – Engage-Test-Aadhar",
+    rc: "default",
+    variants: '[{"Blue / Italian 42":16295835631650}]',
+    dt: "A-Line Mini Dress in Blue",
+    et: 1,
+    act: true,
+    empi: 1775626158114,
+    ts: 1553203672628,
+    du:
+      "https://engage-test-aadhar.myshopify.com/products/a-line-mini-dress-blue",
+    pid: "HUQiyMqiJnnnwJrxvLl+1dgTFlOvum7Rg/FnHdU/v7Y=",
+    epi: 16295835631650,
+    id: "c4837e0f-3f60-4048-837e-0f3f603048e6",
+    ru: "https://engage-test-aadhar.myshopify.com/collections/all",
+    iu:
+      "//cdn.shopify.com/s/files/1/0048/8862/5186/products/2014_10_18_Lana_Look2704.jpg?v=1552997576",
+    dcat1: "Personal computer",
+    pr: 348,
+    ct: "women's dresses",
+    dcat: "Mac"
+  },
+  {
+    di: "37b71b11-b746-4b8f-b539-34a046610f46",
+    pt: "Adania Pant – Engage-Test-Aadhar",
+    rc: "default",
+    variants: '[{"Blue / 36":16295294959650}]',
+    dt: "Adania Pant",
+    et: 4,
+    act: true,
+    empi: 1775522316322,
+    ts: 1553203642982,
+    du: "https://engage-test-aadhar.myshopify.com/products/adania-pant",
+    pid: "HUQiyMqiJnnnwJrxvLl+1dgTFlOvum7Rg/FnHdU/v7Y=",
+    epi: 16295294959650,
+    id: "83e68ee6-dbe1-4f4b-a68e-e6dbe14f4b7a",
+    ru: "https://engage-test-aadhar.myshopify.com/collections/all",
+    iu:
+      "//cdn.shopify.com/s/files/1/0048/8862/5186/products/2015-06-11-Ashley_Look10_50102_23612.jpg?v=1552992028",
+    dcat1: "Personal computer",
+    pr: 368,
+    ct: "Womens Pants",
+    dcat: "Mac"
+  },
+  {
+    di: "37b71b11-b746-4b8f-b539-34a046610f46",
+    pt: "Zig Coat in Evening – Engage-Test-Aadhar",
+    rc: "default",
+    variants: '[{"Evening / Small":16295793688610}]',
+    dt: "Zig Coat in Evening",
+    et: 1,
+    act: true,
+    empi: 1775621832738,
+    ts: 1553233529382,
+    du: "https://engage-test-aadhar.myshopify.com/products/zig-coat-evening",
+    pid: "HUQiyMqiJnnnwJrxvLl+1dgTFlOvum7Rg/FnHdU/v7Y=",
+    epi: 16295793688610,
+    id: "501224cc-bed6-4488-9224-ccbed6f48868",
+    ru:
+      "https://engage-test-aadhar.myshopify.com/collections/all?sort_by=title-descending",
+    iu:
+      "//cdn.shopify.com/s/files/1/0048/8862/5186/products/2014-10-10_Addis_Look_03_03.jpg?v=1552997334",
+    dcat1: "Personal computer",
+    pr: 388,
+    ct: "men's coats & jackets",
+    dcat: "Mac"
+  },
+  {
+    di: "467920c1-3982-4fd4-9f28-baf79c8c1653",
+    variants: '[{"Rose / One Size":16295811448866}]',
+    dt: "Oscar Luggage Tag in Rose",
+    et: 4,
+    act: true,
+    empi: 1775623995426,
+    ts: 1553741378578,
+    du:
+      "http://engage-test-aadhar.myshopify.com/products/oscar-luggage-tag-rose?variant=16295811448866",
+    pid: "HUQiyMqiJnnnwJrxvLl+1dgTFlOvum7Rg/FnHdU/v7Y=",
+    epi: 16295811448866,
+    id: "a062746c-3c6b-4f4d-a274-6c3c6b0f4db0",
+    iu:
+      "https://cdn.shopify.com/s/files/1/0048/8862/5186/products/product_misc_lilith_oscar-luggage-tag_rose01_1024x1024_f693cb85-b9e2-416e-9f80-2c9b96549d0c.jpg?v=1552997428",
+    dcat1: "Unknown",
+    pr: 8,
+    dcat: "unknown "
+  },
+  {
+    di: "37b71b11-b746-4b8f-b539-34a046610f46",
+    pt: "Aliam Skirt – Engage-Test-Aadhar",
+    rc: "default",
+    variants: '[{"X-Small / Mud":16295388217378}]',
+    dt: "Aliam Skirt",
+    et: 1,
+    act: true,
+    empi: 1775540633634,
+    ts: 1553215184347,
+    du: "https://engage-test-aadhar.myshopify.com/products/aliam-skirt-mud",
+    pid: "HUQiyMqiJnnnwJrxvLl+1dgTFlOvum7Rg/FnHdU/v7Y=",
+    epi: 16295388217378,
+    id: "a9f90cfc-9fd4-4402-b90c-fc9fd4a40233",
+    ru: "https://engage-test-aadhar.myshopify.com/collections/all?page=2",
+    iu:
+      "//cdn.shopify.com/s/files/1/0048/8862/5186/products/2015-03-15_Lana_Look_41_30411_13930.jpg?v=1552993428",
+    dcat1: "Personal computer",
+    pr: 509.6,
+    ct: "women's skirts",
+    dcat: "Mac"
+  },
+  {
+    di: "37b71b11-b746-4b8f-b539-34a046610f46",
+    pt: "Adania Pant – Engage-Test-Aadhar",
+    rc: "default",
+    variants: '[{"Blue / 36":16295294959650}]',
+    dt: "Adania Pant",
+    et: 1,
+    act: true,
+    empi: 1775522316322,
+    ts: 1553203618305,
+    du: "https://engage-test-aadhar.myshopify.com/products/adania-pant",
+    pid: "HUQiyMqiJnnnwJrxvLl+1dgTFlOvum7Rg/FnHdU/v7Y=",
+    epi: 16295294959650,
+    id: "067a1f79-c6d2-4ace-ba1f-79c6d20ace65",
+    ru: "https://engage-test-aadhar.myshopify.com/collections/all",
+    iu:
+      "//cdn.shopify.com/s/files/1/0048/8862/5186/products/2015-06-11-Ashley_Look10_50102_23612.jpg?v=1552992028",
+    dcat1: "Personal computer",
+    pr: 368,
+    ct: "Womens Pants",
+    dcat: "Mac"
+  },
+  {
+    di: "37b71b11-b746-4b8f-b539-34a046610f46",
+    pt: "Annie Necklace – Engage-Test-Aadhar",
+    rc: "default",
+    variants: '[{"O/S / Chrysoprase":16295366262818}]',
+    dt: "Annie Necklace",
+    et: 1,
+    act: true,
+    empi: 1775534473250,
+    ts: 1553215138296,
+    du:
+      "https://engage-test-aadhar.myshopify.com/products/annie-necklace-chrysoprase",
+    pid: "HUQiyMqiJnnnwJrxvLl+1dgTFlOvum7Rg/FnHdU/v7Y=",
+    epi: 16295366262818,
+    id: "b6826851-2610-45d8-8268-512610c5d888",
+    ru: "https://engage-test-aadhar.myshopify.com/collections/all?page=3",
+    iu:
+      "//cdn.shopify.com/s/files/1/0048/8862/5186/products/2015-05-01_Accessories_11_23496_21558.jpg?v=1552993072",
+    dcat1: "Personal computer",
+    pr: 111,
+    ct: "women's necklaces",
+    dcat: "Mac"
+  },
+  {
+    di: "37b71b11-b746-4b8f-b539-34a046610f46",
+    pt: "Aliam Skirt – Engage-Test-Aadhar",
+    rc: "default",
+    variants: '[{"X-Small / Black":16295388839970}]',
+    dt: "Aliam Skirt",
+    et: 1,
+    act: true,
+    empi: 1775540731938,
+    ts: 1553215156342,
+    du: "https://engage-test-aadhar.myshopify.com/products/aliam-skirt-black",
+    pid: "HUQiyMqiJnnnwJrxvLl+1dgTFlOvum7Rg/FnHdU/v7Y=",
+    epi: 16295388839970,
+    id: "0b87944b-60f4-45fe-8794-4b60f495fe13",
+    ru: "https://engage-test-aadhar.myshopify.com/collections/all?page=2",
+    iu:
+      "//cdn.shopify.com/s/files/1/0048/8862/5186/products/2015-03-15_Lana_Look_31_23196_13672.jpg?v=1552993436",
+    dcat1: "Personal computer",
+    pr: 509.6,
+    ct: "women's skirts",
+    dcat: "Mac"
+  },
+  {
+    di: "37b71b11-b746-4b8f-b539-34a046610f46",
+    pt: "Aliam Skirt – Engage-Test-Aadhar",
+    rc: "default",
+    variants: '[{"Small / Mud":16295388250146}]',
+    dt: "Aliam Skirt",
+    et: 8,
+    act: true,
+    empi: 1775540633634,
+    ts: 1553215187212,
+    du: "https://engage-test-aadhar.myshopify.com/products/aliam-skirt-mud",
+    pid: "HUQiyMqiJnnnwJrxvLl+1dgTFlOvum7Rg/FnHdU/v7Y=",
+    epi: 16295388250146,
+    id: "54916fbf-c964-44d3-916f-bfc96424d38a",
+    ru: "https://engage-test-aadhar.myshopify.com/collections/all?page=2",
+    iu:
+      "//cdn.shopify.com/s/files/1/0048/8862/5186/products/2015-03-15_Lana_Look_41_30411_13930.jpg?v=1552993428",
+    dcat1: "Personal computer",
+    pr: 509.6,
+    ct: "women's skirts",
+    dcat: "Mac"
+  },
+  {
+    di: "467920c1-3982-4fd4-9f28-baf79c8c1653",
+    variants: '[{"Blue / Italian 42":16295835631650}]',
+    dt: "A-Line Mini Dress in Blue",
+    et: 4,
+    act: true,
+    empi: 1775626158114,
+    ts: 1553737630914,
+    du:
+      "http://engage-test-aadhar.myshopify.com/products/a-line-mini-dress-blue?variant=16295835631650",
+    pid: "HUQiyMqiJnnnwJrxvLl+1dgTFlOvum7Rg/FnHdU/v7Y=",
+    epi: 16295835631650,
+    id: "5aa37ae1-77a8-4bab-a37a-e177a8bbabc9",
+    iu:
+      "https://cdn.shopify.com/s/files/1/0048/8862/5186/products/2014_10_18_Lana_Look2704.jpg?v=1552997576",
+    dcat1: "Unknown",
+    pr: 348,
+    dcat: "unknown "
+  }
+];
+
+export const stubCart = {
+  grand_total: "607.31",
+  line_items: [
+    {
+      fulfillment_service: "manual",
+      fulfillment_status: "unfulfilled",
+      price: 551.6,
+      product_id: 1775533752354,
+      quantity: 1,
+      requires_shipping: true,
+      sku: "23516",
+      taxable: true,
+      title: "3/4 Sleeve Kimono Dress",
+      variant_id: 16295364165666,
+      variant_title: "10",
+      vendor: "Antoni & Alison"
+    }
+  ],
+  subtotal: "551.60",
+  tax_total: "55.71",
+  customer: {
+    email: "some@example.com"
+  }
+};
+if (!window.ShopifyStoreAPI) {
+  window.ShopifyStoreAPI = { cache: {}, _epis: {} };
+
+  window.ShopifyStoreAPI.getProductForDu = function(du, cb) {
+    var duCache = window.ShopifyStoreAPI.cache[du] || {
+      loaded: false,
+      cbs: [],
+      loading: false
+    };
+    if (duCache.loaded) {
+      cb.success(duCache.val);
+    } else if (duCache.loading) {
+      duCache.cbs.push(cb);
+    } else {
+      duCache.cbs.push(cb);
+      duCache.loading = true;
+      fetch(du.split("?")[0] + ".js")
+        .then(
+          function(response) {
+            return response.json();
+          },
+          function(error) {
+            window.showErrorAndExit("getProduct 1", error, cb);
+          }
+        )
+        .then(function(responseJson) {
+          responseJson.variants.forEach(function(v) {
+            v.iu = v.featured_image
+              ? v.featured_image
+              : responseJson.featured_image;
+            v.pr = v.price / 100;
+            v.epi = v.id;
+            v.variants = v.title;
+            window.ShopifyStoreAPI._epis[v.epi] = v;
+          });
+          if (window.CFG && window.CFG.POSLocation) {
+            //window.CFG.POSLocation.locationId;
+            // fetch locationId
+            duCache.loading = duCache.loaded = true;
+            duCache.val = responseJson;
+            duCache.cbs.forEach(function(cbx) {
+              cbx.success(responseJson);
+            });
+            duCache.cbs = [];
+          } else {
+            duCache.loading = duCache.loaded = true;
+            duCache.val = responseJson;
+            duCache.cbs.forEach(function(cbx) {
+              cbx.success(responseJson);
+            });
+            duCache.cbs = [];
+          }
+        })
+        .catch(function(error) {
+          window.showErrorAndExit("getProduct 2", error, cb);
+          throw error;
+        });
+    }
+  };
+}
+export const updateArr = (arr, et) => {
+  let arrMap = arr.map(e => {
+    return e.epi;
+  });
+  let idx = arrMap.indexOf(et.epi);
+  if (idx > -1) {
+    arr[idx] = et;
+  } else {
+    arr.push(et);
+  }
+  return arr;
+};
+
+if (!window.ShopifyPOS) {
+  window._LTracker = [];
+  window.CFG = { POSCart: stubCart };
+  window.ShopifyPOSAPI = {};
+
+  window.ShopifyPOSAPI.addLineItemToCart = function(epi, cb) {
+    setTimeout(function() {
+      cb.success();
+    }, 1500);
+  };
+
+  window.ShopifyPOSAPI.removeLineItemFromCart = function(idx, cb) {
+    setTimeout(function() {
+      cb.success();
+    }, 1500);
+  };
+  window.ShopifyPOSAPI.loadCart = function(cb) {
+    setTimeout(function() {
+      cb.success();
+    }, 1500);
+  };
+  window.SwymAPI = {};
+  window.SwymAPI.getEventsForUser = function(cb) {
+    setTimeout(function() {
+      cb.success(stubItems);
+    }, 1500);
+  };
+  window.ShopifyPOSAPI.loadCart({
+    success: function() {
+      // all ready
+      // render stuff
+      document.dispatchEvent(new CustomEvent("swym:ready"));
+    }
+  });
+
+  window.CFG.isPOS = true;
+
+  window.handleClose = function(){
+
+  };
+  // window.CFG.showCartInfo = true;
+  // window.CFG.showCartActions = false;
+  // window.CFG.showEts = false;
+  // window.CFG.showEtActions = false;
+}
---
+++ pospwa/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {AppProvider} from "@shopify/polaris";
+import * as serviceWorker from './serviceWorker';
+
+import App from "./App";
+import "@shopify/polaris/styles.css";
+
+ReactDOM.render(<AppProvider><App /></AppProvider>, document.getElementById("root"));
+
+serviceWorker.register();
